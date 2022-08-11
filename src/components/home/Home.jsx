885e498aff401e550bb89140ac52d141c709da63
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-08-11 03:59:16 +0100

    fetched all the projects in the home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,24 +1,69 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
+import { Button, Container, Row } from "react-bootstrap";
+import Loader from "../Loader";
+import SingleProject from "./SingleProject";
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [projects, setProjects] = useState([{},{}]);
+  useEffect(() => {
+    fetchProjects();
+  }, []);
 
-    useEffect(()=> {
-        fetchProjects()
-    },[])
-
-    const fetchProjects = async () => {
-        try {
-            const res = await fetch(`${process.env.REACT_APP_PROD_BE_UR}/projects`)
-        } catch (error) {
-            
+  const fetchProjects = async () => {
+    const baseUrl = "http://localhost:3001"
+    try {
+      const response = await fetch(
+        `${baseUrl}/projects`,
+        {
+          method: "GET",
+          headers: {
+            "authorization": localStorage.getItem("MyToken"),
+          },
         }
+      );
+      if (response.status !== 200) {
+        const data = await response.json();
+        console.log(data);
+        setError("Error in fetching projects");
+        setIsLoading(false);
+      } else {
+        const data = await response.json();
+        setProjects(data.projects);
+        console.log(data.projects.reverse());
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+      setError("Could not fetch projects");
     }
+  };
 
   return (
     <div>
-        
+      <Container className="">
+          <h1 >Projects</h1>
+          <Button >Create new project</Button>
+        <Row>
+          {
+        isLoading ? (
+        <Loader />
+         ) : 
+        error ? (
+            <div>{error}</div>
+           ) : (
+            projects.map((project) => (
+              <SingleProject key={project._id} project={project} />
+            )
+            )
+            )
+          }
+        </Row>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
